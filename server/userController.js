1d30c62a01b49cadea7d107885960dee64b7eb90
Nick Kingston <ngrifk@gmail.com>
2023-07-13 13:08:49 -0500

    security-lecture-first commit

--- server/userController.js
+++ server/userController.js
@@ -2,50 +2,62 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   signup: (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body;
+    const saltRounds = 10;
 
-    let newDatabaseEntry = {}
-    newDatabaseEntry.email = email
-    newDatabaseEntry.password = password
-    newDatabaseEntry.destiny = destinies[Math.floor(Math.random() * destinies.length)]
-    console.log('\nNew database entry: ')
-    console.log(newDatabaseEntry)
-    database.push(newDatabaseEntry)
-    res.status(200).send({success: true})
+    bcrypt.hash(password, saltRounds, (err, passwordHash) => {
+      if (err) {
+        console.error('Error hashing password:', err);
+        res.status(500).send({ success: false, message: 'Error signing up' });
+        return;
+      }
+
+      let newDatabaseEntry = {};
+      newDatabaseEntry.email = email;
+      newDatabaseEntry.passwordHash = passwordHash;
+      newDatabaseEntry.destiny = destinies[Math.floor(Math.random() * destinies.length)];
+
+      console.log('\nNew database entry: ');
+      console.log(newDatabaseEntry);
+
+      database.push(newDatabaseEntry);
+      res.status(200).send({ success: true });
+    });
   },
   login: (req, res) => {
-    const {email, password} = req.body
-    let userData
+    const { email, password } = req.body;
+    let userData;
 
-    for (let i=0; i<database.length; i++) {
-      if (email === database[i].email && password === database[i].password) {
-        userData = database[i]
+    for (let i = 0; i < database.length; i++) {
+      if (email === database[i].email) {
+        if (bcrypt.compareSync(password, database[i].passwordHash)) {
+          userData = database[i];
+          break;
+        }
       }
     }
 
     if (!userData) {
-      res.status(200).send({success: false, message: 'bad password or username'})
+      res.status(200).send({ success: false, message: 'Bad password or username' });
     } else {
-      const destinyIntro = "Your final destiny is to "
-      res.status(200).send({success: true, destiny: userData.destiny, intro: destinyIntro})
+      const destinyIntro = 'Your final destiny is to ';
+      res.status(200).send({ success: true, destiny: userData.destiny, intro: destinyIntro });
     }
   }
-}
-
+};
 
 const database = [
   {
-    email:'john@gmail.com',
-    password:'1234asdf',
-    destiny:'becoming the new Santa'
+    email: 'john@gmail.com',
+    passwordHash: '$2b$10$HzeKYq7PcQl17h1u0RPRa.j21cXHfnk3xYr./x2mF24C8STe5gEgC',
+    destiny: 'becoming the new Santa'
   },
   {
-    email:'sallybonnet@yahoo.com',
-    passwordHash:'funpassword',
-    destiny:'becoming best friends with Martha Stewart'
+    email: 'sallybonnet@yahoo.com',
+    passwordHash: '$2b$10$aaNK.wu/6CIlIRXF9ooelK.nO9.6NPQYdzEax8tpyTDU16A5ZsA5ZO',
+    destiny: 'becoming best friends with Martha Stewart'
   },
-]
-
+];
 
 const destinies = [
   'become a well-respected bartender',
@@ -54,5 +66,5 @@ const destinies = [
   'become a hoarder',
   'become the lead in a high-budget film, filling in for Henry Cavill when he unexpectedly goes MIA',
   'summit Everest',
-  'become the next Bruce Willis',
-]
+  'become the next Bruce Willis'
+];
